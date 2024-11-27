720737ac3fd5d09ecfca750af09fee570107071a
ascpixi <44982772+ascpixi@users.noreply.github.com>
2024-11-27 21:55:51 +0100

    chore: move host functions into their own header file

--- src/coreclr/jit/libryujit/interop.cpp
+++ src/coreclr/jit/libryujit/interop.cpp
@@ -3,46 +3,11 @@
 #include "corjithost.h"
 #include "corjit.h"
 #include "interopjitinfo.h"
+#include "host.h"
 
 #ifndef DLLEXPORT
     #define DLLEXPORT
 #endif // !DLLEXPORT
-
-/// These functions must be implemented by the consumers of the static library.
-
-// Allocate memory of the given size in bytes.
-//      Passed to: `ICorJitHost::allocateMemory`
-extern "C" void* ryujit_host_alloc(size_t size);
-
-// Frees memory previous obtained by a call to `ryujit_alloc`.
-//      Passed to: `ICorJitHost::freeMemory`
-extern "C" void ryujit_host_free(void* block);
-
-// Returns an integer config value for the given key, if any exists.
-//      Passed to: `ICorJitHost::getIntConfigValue`
-extern "C" int ryujit_host_get_int_cfgval(
-    const char* name,
-    int defaultValue
-);
-
-// Return a string config value for the given key, if any exists.
-//      Passed to: `ICorJitHost::getStringConfigValue`
-extern "C" const char* ryujit_host_get_string_cfgval(const char* name);
-
-// Free a string ConfigValue returned by the runtime. JITs using the getStringConfigValue
-// query are required to return the string values to the runtime for deletion.
-// This avoids leaking the memory in the JIT.
-//      Passed to: `ICorJitHost::freeStringConfigValue`
-extern "C" void ryujit_host_free_string_cfgval(const char* value);
-
-// Allocate memory slab of the given size in bytes. The host is expected to pool
-// these for good performance.
-//      Passed to: `ICorJitHost::allocateSlab`
-extern "C" void* ryujit_host_alloc_slab(size_t size, size_t* pActualSize);
-
-// Free memory slab of the given size in bytes.
-//      Passed to: `ICorJitHost::freeSlab`
-extern "C" void ryujit_host_free_slab(void* slab, size_t actualSize);
 
 class RyuJitHost : public ICorJitHost
 {
